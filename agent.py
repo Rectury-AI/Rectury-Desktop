d241c342854173e02041aba27fa880092bf9e5de
Jan Aymerich Mateo <janayme15@gmail.com>
2026-06-15 19:55:36 +0200

    feat: connect grok api

--- agent.py
+++ agent.py
@@ -1,11 +1,24 @@
-import openai
+import os
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.getenv("XAI_API_KEY")
 
-client = openai(
+client = OpenAI(
     api_key=api_key,
     base_url="https://api.x.ai/v1"
 )
+while True:
+    user= input("Tú:").strip()
+    response = client.responses.create(
+        model="grok-4.3",
+        input=user
+    )
+    print(response.output_text)
+    
+    
+    
+    if user == "":
+        break
